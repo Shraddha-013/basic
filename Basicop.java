713ae8d98331f74d62ef3e3b5a4c9aa0003914ee
Shraddha-013 <sjshraddha13@gmail.com>
2025-05-06 10:09:41 +0530

    Saved

---
+++ Basicop.java
@@ -0,0 +1,11 @@
+public class Basicop {
+    public static void main(String[] args) {
+        int a = 15, b = 5;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b; 
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+    }
+}       
